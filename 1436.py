0d7407b63ba8c2aecea9e4c7d7ef43b02d8d569a
Begachu <ygo65312@naver.com>
2021-01-08 13:43:15 +0900

    영화감독 숌(1436번): 성공
    앞부분을 stack이라는 변수를 두어 사용. 해당 변수는 666의 앞자리와 연관이 있다. 분기는 총 3개로 나눔, stack의
    일의 자리가 0~5일때와 6일때, 7~9일때로, 0~5와 7~9는 같은 방법을 공유한다.
    6의 경우 해당 숫자의 6반복 횟수-3하여 해당 값이 k라고 할 때 10^k만큼의 범위를 더 구하도록 하였다.

--- 1436.py
+++ 1436.py
@@ -16,20 +16,24 @@ while N>0:
         break
     
     stack += 1
-    count = '6'
+    count = 10
+    _minus = '6'
     temp = stack//10
     result += 1000
     while temp%10==6:
         temp = temp//10
-        count += '6'
-    count = int(count)
-    result -= count
-    if N<=count+1:
+        count *= 10
+        _minus += '6'
+    
+    _save_ = result
+    result -= int(_minus) 
+
+    if N<=count:
         result += N-1
         break
     
-    result += count
-    N -= count+1
+    N -= count
+    result = _save_
 
     temp = N%3
     temp2 = N/3
