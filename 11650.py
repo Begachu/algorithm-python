066b2e6ed1515efcb2e4fba83d0cff80359b7e3c
Begachu <ygo65312@naver.com>
2021-01-08 16:16:33 +0900

    좌표 정렬하기(11650번): 성공
    정렬 규칙을 두개 주어 수행(sorted()함수 사용)

---
+++ 11650.py
@@ -0,0 +1,17 @@
+# 좌표 정렬하기
+
+import sys
+
+N = int(sys.stdin.readline())
+array = []
+
+for i in range(N):
+    _input = sys.stdin.readline().split(' ')
+    _x = int(_input[0])
+    _y = int(_input[1])
+    array.append((_x, _y))
+
+result = sorted(array, key=lambda n : (n[0], n[1]))
+
+for i in range(N):
+    print(result[i][0],result[i][1])
