7bfe4117df2fccb5893cea1c8a9edbe1b8962024
Begachu <ygo65312@naver.com>
2021-01-22 09:41:50 +0900

    풍선 맞추기(11509번): 성공
    불필요한 계산 삭제함.(배열의 시작을 -1이 아닌 0으로 함)

--- 11509.py
+++ 11509.py
@@ -1,9 +1,9 @@
 # 풍선 맞추기
-save = [-1]*1000001
+save = [0]*1000001
 N = int(input())
 balloons = list(map(int, input().split(' ')))
 for i in range(N):
-    if save[balloons[i]]!=-1:
+    if save[balloons[i]]!=0:
         save[balloons[i]] -= 1
     save[balloons[i]-1] += 1
-print(sum(save)+1000001)
+print(sum(save))
