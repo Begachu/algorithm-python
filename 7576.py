49f64e3738aacbf894da5db7441ba572da3091b7
Begachu <ygo65312@naver.com>
2021-01-28 14:21:28 +0900

    토마토(7576번) : 성공
    각각 1을 start node로 두고 bfs 탐색을 하면 해결됨! 이후 가장 큰 값을 저장한 후 반환하면 끝. 추가적으로 모든
    토마토에 대해 탐색했는지를 알아보기 위해서 전체 익어야하는 토마토의 수와 익은 토마토의 수를 저장할 필요가 있었음

--- 7576.py
+++ 7576.py
@@ -1,1 +1,53 @@
 # 토마토
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+(M, N) = list(map(int,input().split()))
+node = []    # start node
+amount = M*N
+cnt = [[-2 for m in range(M+2)] for n in range(N+2)]
+
+for n in range(N):
+    temp = list(map(int, input().split()))
+    for i in range(M):
+        if temp[i]==-1:
+            temp[i] = -2
+            amount -= 1
+        else:
+            if temp[i]==1:
+                node.append((n+1, i+1, 0))
+                temp[i] = 0
+                amount -= 1
+            else:
+                temp[i] = -1
+    cnt[n+1][1:M+1] = temp
+
+queue = deque(node)     # for BFS
+_max = -1
+while len(queue)!=0:
+    temp = queue.popleft()
+    cnt[temp[0]][temp[1]] = temp[2]
+    if _max< temp[2]:
+        _max = temp[2]
+
+    if cnt[temp[0]-1][temp[1]]==-1:
+        cnt[temp[0]-1][temp[1]] = -2
+        amount -= 1
+        queue.append((temp[0]-1, temp[1], temp[2]+1))
+    if cnt[temp[0]+1][temp[1]]==-1:
+        cnt[temp[0]+1][temp[1]] = -2
+        amount -= 1
+        queue.append((temp[0]+1, temp[1], temp[2]+1))
+    if cnt[temp[0]][temp[1]-1]==-1:
+        cnt[temp[0]][temp[1]-1] = -2
+        amount -= 1
+        queue.append((temp[0], temp[1]-1, temp[2]+1))
+    if cnt[temp[0]][temp[1]+1]==-1:
+        cnt[temp[0]][temp[1]+1] = -2
+        amount -= 1
+        queue.append((temp[0], temp[1]+1, temp[2]+1))
+if amount!=0:
+    print(-1)
+else:
+    print(_max)
